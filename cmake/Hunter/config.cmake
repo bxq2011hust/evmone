37799f8226c72e2e64e59583f07ab834868de33e
Xingqang Bai <bxq2011hust@qq.com>
2021-04-06 19:19:31 +0800

    use specific intx (#3)

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -3,6 +3,13 @@
 # Licensed under the Apache License, Version 2.0.
 
 hunter_config(
+    intx
+    VERSION 0.4.1
+    URL https://github.com/chfast/intx/archive/v0.4.0.tar.gz
+    SHA1 8a2a0b0efa64899db972166a9b3568a6984c61bc
+)
+
+hunter_config(
     GTest
     VERSION 1.8.1
     URL https://github.com/google/googletest/archive/release-1.8.1.tar.gz
